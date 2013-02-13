47af505544b677382b13f707f43edf36bf07a3c1
Sergey Larionov <sergey@larionov.biz>
2013-02-13 22:15:18 +1100

    Repin qiwi soap client submodule

--- protected/config/_routes.php
+++ protected/config/_routes.php
@@ -2,7 +2,6 @@
 
 return array(
 	'/'                                           => 'main/index',
-	'/test/'                                      => 'main/test',
 
 	'/<_c:qiwi>/<_a:bill|success|fail|callback>/' => '<_c>/<_a>',
 );
